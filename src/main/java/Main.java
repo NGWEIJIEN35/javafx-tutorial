4101f8e54289546f73a20367189b13dea156ee81
NGWEIJIEN35 <254315756+NGWEIJIEN35@users.noreply.github.com>
2026-09-05 17:49:46 +0800

    Complete JavaFX tutorial part 3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,6 +19,7 @@ public class Main extends Application {
     private Scene scene;
     private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
     private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+    private Duke duke = new Duke();
 
     @Override
     public void start(Stage stage) {
@@ -26,11 +27,18 @@ public class Main extends Application {
         dialogContainer = new VBox();
         scrollPane.setContent(dialogContainer);
 
+        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
+
         userInput = new TextField();
         sendButton = new Button("Send");
 
-        DialogBox dialogBox = new DialogBox("Hello!", userImage);
-        dialogContainer.getChildren().addAll(dialogBox);
+
+        sendButton.setOnMouseClicked((event) -> {
+            handleUserInput();
+        });
+        userInput.setOnAction((event) -> {
+            handleUserInput();
+        });
 
         AnchorPane mainLayout = new AnchorPane();
         mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
@@ -68,5 +76,15 @@ public class Main extends Application {
 
         stage.setScene(scene); // Setting the stage to show our scene
         stage.show(); // Render the stage.
+    }
+
+    private void handleUserInput() {
+        String userText = userInput.getText();
+        String dukeText = duke.getResponse(userInput.getText());
+        dialogContainer.getChildren().addAll(
+                DialogBox.getUserDialog(userText, userImage),
+                DialogBox.getDukeDialog(dukeText, dukeImage)
+        );
+        userInput.clear();
     }
 }
